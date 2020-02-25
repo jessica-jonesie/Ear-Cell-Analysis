13b4bce527a83d98d0cf2a39f844fa8d2e1f27dd
Connor <connor.healy@utah.edu>
2020-02-25 13:01:56 -0700

    Added Rayleigh Test Statistic for uniformity

--- VestibularSystemVisualization.m
+++ VestibularSystemVisualization.m
@@ -26,5 +26,15 @@ PolarityMaps(CellProps,ImDat,clrMap);
 [combinedX,intCDFs] = InterpCDFs(x,CDF);
 interpCDFPlot(combinedX,intCDFs,'polar');
 
+% Komogorov Smirnov - Tests the null hypothesis that two populations are
+% drawn from the same distribution aka. tests whether two distributions are
+% the same/similar. Note KS is not necessarily appropriate for circular
+% statistics.
 DistType ={'E','E','E','T'}; % Is the distribution empirical or theoretical.
 [KSResults] = KSStruct(x,CDF,DistType);
+
+% Rayleigh Test for Uniformity. Tests the null hypothesis that the input
+% distribution is uniform.
+RayPValue_Combo = RayleighTest(CellProps.NormOrientation);
+RayPValue_Support = RayleighTest(CellProps.NormOrientation(CellProps.Type=='S'));
+RayPValue_Hair = RayleighTest(CellProps.NormOrientation(CellProps.Type=='H'));

7d2c6fa43c78d4bc884643632f9e83198d7194ae
Connor <connor.healy@utah.edu>
2020-02-26 14:37:13 -0700

    Incorporated Double Angle transformation prior to the calculation of circular statistics

--- VestibularSystemVisualization.m
+++ VestibularSystemVisualization.m
@@ -10,9 +10,7 @@ clrMap = 'RdYlBu';
 [file,path] = uigetfile('*.mat');
 load(fullfile(path,file));
 
-AngsCombo = CellProps.NormOrientation;
-AngsHair = CellProps.NormOrientation(CellProps.Type=='Hair');
-AngsSupport = CellProps.NormOrientation(CellProps.Type=='Support');
+
 
 %% Display Results
 PolAndOriHistograms(CellProps,'Full')
@@ -30,18 +28,30 @@ PolarityMaps(CellProps,ImDat,clrMap);
 [combinedX,intCDFs] = InterpCDFs(x,CDF);
 interpCDFPlot(combinedX,intCDFs,'polar');
 
-% Komogorov Smirnov - Tests the null hypothesis that two populations are
+% Kolmogorov Smirnov - Tests the null hypothesis that two populations are
 % drawn from the same distribution aka. tests whether two distributions are
 % the same/similar. Note KS is not necessarily appropriate for circular
 % statistics.
 DistType ={'E','E','E','T'}; % Is the distribution empirical or theoretical.
 [KSResults] = KSStruct(x,CDF,DistType);
 
+% For circular statistics note that the diametrically bimodal orientation
+% measurements must be first transformed using the double angle
+% transformation.
+CellProps.DblAngOrientation = DblAngTransform(CellProps.NormOrientation,'deg');
+
+figure
+DblAngPlot(CellProps.NormOrientation,'deg',36)
+
+AngsCombo = CellProps.DblAngOrientation;
+AngsHair = CellProps.DblAngOrientation(CellProps.Type=='Hair');
+AngsSupport = CellProps.DblAngOrientation(CellProps.Type=='Support');
 % Rayleigh Test for Uniformity. Tests the null hypothesis that the input
 % distribution is uniform.
 RayPValue_Combo = RayleighTest(AngsCombo);
 RayPValue_Support = RayleighTest(AngsSupport);
 RayPValue_Hair = RayleighTest(AngsHair);
+
 
 % Mardia-Watson-Wheel Uniform-Scores Test
 % This procedure tests whether two distributions (including circular

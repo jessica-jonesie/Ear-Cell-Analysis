adf8570d094f8f57c8461f87da8d0cdafbac825a
Connor <connor.healy@utah.edu>
2020-05-29 17:19:14 -0600

    Added input parsing scheme and switch/case to compute influence based on minimum distance in addition to the cumulative influence based upon the inverse square of the distance or a decay function of the distance.

--- Sourced Functions/pt2ptInfluence.m
+++ Sourced Functions/pt2ptInfluence.m
@@ -1,25 +1,83 @@
-function [refMagnitude,refAngle,aveX,aveY] = pt2ptInfluence(respPts,refPts)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [refMagnitude,refAngle,aveX,aveY] = pt2ptInfluence(respPts,refPts,TransformType,varargin)
+%PT2PTINFLUENCE Compute cumulative influence vector. 
+%   PT2PTINFLUENCE computes a vector to represent the cumulative influence
+%   of a set of reference points (refPts) upon a set of response points
+%   (resPts). A vector is computed for each response point based on it's
+%   distance with respect to the reference points (or a transformation of
+%   that distance e.g. inverse square, exponential, etc..). PT2PTINFLUENCE
+%   outputs the magnitude, angle/direction, and components of the influence
+%   vectors for each response point. 
+%
+%   The type of transformation applied to the distance can be specified
+%   with the 'TransformType' input variable. The options are 'None' in
+%   which a response points influence vector is defined by the reference
+%   point nearest to each response point, 'InverseSquare' in the inverse
+%   square of the euclidean distance between the response point and each
+%   reference point is taken then averaged to obtain a single average
+%   influence vector. 'ExponentialDecay' is similar to 'InverseSquare' except
+%   the a negative exponential function is used to define the influences.
+%
+%   [Magnitude,Angle] = pt2ptInfluence(respPts,refPts,'none') computes the
+%   Magnitude and Angle of the reference vectors for each of the response
+%   points with respect to the reference points using the minimal 
 
+%% parse inputs
+p = inputParser;
+defaultTransform = 'none';
+validTransforms = {'none','inverse','decay'};
+checkTransform = @(x) any(validatestring(x,validTransforms));
+
+defaultStrength = 1;
+
+addRequired(p,'respPts',@isnumeric)
+addRequired(p,'refPts',@isnumeric)
+addRequired(p,'TransformType',checkTransform)
+addOptional(p,'Strength',defaultStrength,@isnumeric)
+
+parse(p,respPts,refPts,TransformType,varargin{:})
+
+%%
 pt2ptDist = pairdist(respPts,refPts);
 pt2ptAng = pairangle2D(respPts,refPts); 
 
-distSquare = pt2ptDist.^2;
-xcomps = cosd(pt2ptAng)./distSquare;
-ycomps = sind(pt2ptAng)./distSquare;
+transformStrength = p.Results.Strength;
 
-% distSquare = exp(-0.2.*pt2ptDist);
-% xcomps = cosd(pt2ptAng).*distSquare;
-% ycomps = sind(pt2ptAng).*distSquare;
+switch TransformType
+    case 'inverse'
+        distSquare = pt2ptDist.^transformStrength;
+        xcomps = cosd(pt2ptAng)./distSquare;
+        ycomps = sind(pt2ptAng)./distSquare;
 
-xcomps(xcomps==Inf)=0;
-ycomps(ycomps==Inf)=0;
+        xcomps(xcomps==Inf)=0;
+        ycomps(ycomps==Inf)=0;
 
-aveX = mean(xcomps,2,'omitnan');
-aveY = mean(ycomps,2,'omitnan');
+        aveX = mean(xcomps,2,'omitnan');
+        aveY = mean(ycomps,2,'omitnan');
+        
+        refAngle = atan2d(aveY,aveX);
+        refMagnitude = sqrt(aveX.^2+aveY.^2);
+    case 'decay'
+        distSquare = exp(-(1/transformStrength).*pt2ptDist);
+        xcomps = cosd(pt2ptAng).*distSquare;
+        ycomps = sind(pt2ptAng).*distSquare;
 
-refAngle = atan2d(aveY,aveX);
-refMagnitude = sqrt(aveX.^2+aveY.^2);
+        xcomps(xcomps==Inf)=0;
+        ycomps(ycomps==Inf)=0;
+
+        aveX = mean(xcomps,2,'omitnan');
+        aveY = mean(ycomps,2,'omitnan');
+        
+        refAngle = atan2d(aveY,aveX);
+        refMagnitude = sqrt(aveX.^2+aveY.^2);
+    case 'none'
+        [mindists,minI] = min(pt2ptDist');
+        refAngle = pt2ptAng(minI)';
+        refMagnitude = ones(length(refAngle),1);
+        aveX = cosd(refAngle);
+        aveY = sind(refAngle);
+        
+end
+
+
 end
 

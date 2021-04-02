d215687bb6b44d29fa5742504edd89aa2c224525
Connor <connor.healy@utah.edu>
2021-04-02 16:18:53 -0600

    Updated to include BuildMask to fix errors related to mask and property tables not having the same number of entries.

--- Sourced Functions/OrientationMaps.m
+++ Sourced Functions/OrientationMaps.m
@@ -3,9 +3,14 @@ function [] = OrientationMaps(CellProps,ImDat,clrMap)
 %   Detailed explanation goes here
 %% Hair Cell Orientation
 data = CellProps.NormOrientation(CellProps.Type=='H');
-bwIm = ImDat.HairCellMask;
+try
+    bwIm = ImDat.HairCellMask;
+    [datmap,fH,axH] = DataMap(bwIm,abs(data));
+catch
+    bwIm = BuildMask(ImDat.HairCellMask,CellProps(CellProps.Type=='H',:));
+    [datmap,fH,axH] = DataMap(bwIm,abs(data));
+end
 
-[datmap,fH,axH] = DataMap(bwIm,abs(data));
 colormap(flipud(brewermap([],clrMap)))
 cbar= colorbar;
 caxis([0 180])
@@ -14,8 +19,8 @@ ylabel(cbar,'Orientation with Respect to Utricular Boundary');
 title('Hair Cell Orientation')
 
 %% Hair Cell Orientation Blurred
-data = CellProps.NormOrientation(CellProps.Type=='H');
-bwIm = ImDat.HairCellMask;
+% data = CellProps.NormOrientation(CellProps.Type=='H');
+% bwIm = ImDat.HairCellMask;
 
 [datmap,fH,axH] = DataMap(bwIm,abs(data),'Disk',70);
 colormap(flipud(brewermap([],clrMap)))
@@ -27,9 +32,14 @@ title('Hair Cell Orientation Blurred')
 
 %% Support Cell Orientation
 data = CellProps.NormOrientation(CellProps.Type=='S');
-bwIm = ImDat.SupportCellMask;
-
-[datmap,fH,axH] = DataMap(bwIm,abs(data));
+try
+    bwIm = ImDat.SupportCellMask;
+    [datmap,fH,axH] = DataMap(bwIm,abs(data));
+catch
+    bwIm = BuildMask(ImDat.HairCellMask,CellProps(CellProps.Type=='S',:));
+    [datmap,fH,axH] = DataMap(bwIm,abs(data));
+end
+    
 colormap(flipud(brewermap([],clrMap)))
 cbar= colorbar;
 caxis([0 180])
@@ -38,8 +48,8 @@ ylabel(cbar,'Orientation with Respect to Utricular Boundary');
 title('Support Cell Orientation')
 
 %% Support Cell Orientation
-data = CellProps.NormOrientation(CellProps.Type=='S');
-bwIm = ImDat.SupportCellMask;
+% data = CellProps.NormOrientation(CellProps.Type=='S');
+% bwIm = ImDat.SupportCellMask;
 
 [datmap,fH,axH] = DataMap(bwIm,abs(data),'Disk',70);
 colormap(flipud(brewermap([],clrMap)))
--- Sourced Functions/PolarityMaps.m
+++ Sourced Functions/PolarityMaps.m
@@ -3,9 +3,14 @@ function [] = PolarityMaps(CellProps,ImDat,clrMap)
 %   Detailed explanation goes here
 %% Hair Cell Polarity
 data = CellProps.CombinedPolarity(CellProps.Type=='H');
-bwIm = ImDat.HairCellMask;
+try
+    bwIm = ImDat.HairCellMask;
+    [datmap,fH,axH] = DataMap(bwIm,data);
+catch
+    bwIm = BuildMask(ImDat.HairCellMask,CellProps(CellProps.Type=='H',:));
+    [datmap,fH,axH] = DataMap(bwIm,abs(data));
+end
 
-[datmap,fH,axH] = DataMap(bwIm,data);
 colormap(flipud(brewermap([],clrMap)))
 cbar= colorbar;
 
@@ -13,8 +18,8 @@ ylabel(cbar,'Magnitude of Intracellular Polarity');
 title('Hair Cell MIP')
 
 %% Hair Cell Polarity Blurred
-data = CellProps.CombinedPolarity(CellProps.Type=='H');
-bwIm = ImDat.HairCellMask;
+% data = CellProps.CombinedPolarity(CellProps.Type=='H');
+% bwIm = ImDat.HairCellMask;
 
 [datmap,fH,axH] = DataMap(bwIm,data,'Disk',70); 
 colormap(flipud(brewermap([],clrMap)))
@@ -23,18 +28,25 @@ cbar= colorbar;
 ylabel(cbar,'Magnitude of Intracellular Polarity (Blurred)');
 title('Hair Cell MIP Blurred')
 %% Support Cell Polarity
-data = CellProps.CombinedPolarity(CellProps.Type=='S');
-bwIm = ImDat.SupportCellMask;
+    data = CellProps.CombinedPolarity(CellProps.Type=='S');
+try
+    bwIm = ImDat.SupportCellMask;
+    [datmap,fH,axH] = DataMap(bwIm,data);
+catch
+    bwIm = BuildMask(ImDat.HairCellMask,CellProps(CellProps.Type=='S',:));
+    [datmap,fH,axH] = DataMap(bwIm,abs(data));
+end
 
-[datmap,fH,axH] = DataMap(bwIm,data);
+
+
 colormap(flipud(brewermap([],clrMap)))
 cbar= colorbar;
 
 ylabel(cbar,'Magnitude of Intracellular Polarity');
 title('Support Cell MIP')
 %% Support Cell Polarity Blurred
-data = CellProps.CombinedPolarity(CellProps.Type=='S');
-bwIm = ImDat.SupportCellMask;
+% data = CellProps.CombinedPolarity(CellProps.Type=='S');
+% bwIm = ImDat.SupportCellMask;
 
 [datmap,fH,axH] = DataMap(bwIm,data,'Disk',70);
 colormap(flipud(brewermap([],clrMap)))

76f7c4f08ce86941440930e46b7825719228e206
Connor <connor.healy@utah.edu>
2021-04-19 15:31:11 -0600

    Added an optional input to turn off plotting. Also updated to work with DataMapSlider

--- Sourced Functions/DataMap.m
+++ Sourced Functions/DataMap.m
@@ -1,6 +1,20 @@
-function [datmap,figHandle,axHandle,labels] = DataMap(bwIm,stat,varargin)
+function [datmap,axHandle,labels] = DataMap(bwIm,stat,varargin)
 %Color labels according to an input statistic
 %   Detailed explanation goes here
+%% parse
+p = inputParser;
+addRequired(p,'bwIm',@islogical)
+addRequired(p,'stat',@isnumeric)
+addParameter(p,'Display',true,@islogical);
+addParameter(p,'BlurType','none',@ischar);
+addParameter(p,'BlurValue',10,@isnumeric);
+
+parse(p,bwIm,stat,varargin{:});
+
+DoDisplay = p.Results.Display;
+BlurType = p.Results.BlurType;
+BlurValue = p.Results.BlurValue;
+%%
 [labels, n] = bwlabel(bwIm);
 
 if n~=length(stat)
@@ -14,18 +28,23 @@ for k=1:n
 end
 
 % Optional blurring
-if nargin>2 && length(varargin)==2
-    if strcmp(varargin{1},'Gaussian')
-        imFilter = fspecial('gaussian',varargin{2},varargin{2});
-    elseif strcmp(varargin{1},'Disk')
-        imFilter = fspecial('disk',varargin{2});
+if ~strcmp(BlurType,'none')
+    if strcmp(BlurType,'Gaussian')
+        imFilter = fspecial('gaussian',BlurValue,BlurValue);
+    elseif strcmp(BlurType,'Disk')
+        imFilter = fspecial('disk',BlurValue);
     else
         error('Invalid filter type')
     end
     datmap = nanconv(datmap,imFilter,'edge');
 end
 
-figHandle = figure;
-axHandle = pcolor(flipud(datmap));
-set(axHandle, 'EdgeColor', 'none')
+% figHandle = figure;
+if DoDisplay
+    axHandle = pcolor(flipud(datmap));
+    set(axHandle, 'EdgeColor', 'none')
+else
+    axHandle = [];
+end
+
 end

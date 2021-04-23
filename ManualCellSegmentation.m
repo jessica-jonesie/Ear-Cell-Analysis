b4af6e3d9893e04606f417211231849f3f97a03f
Connor <connor.healy@utah.edu>
2021-04-23 11:52:20 -0600

    Updated to make input images more consistent. Now requires a mask labeling hair cells, support cells, cell boundaries, and polar bodies.

--- ManualCellSegmentation.m
+++ ManualCellSegmentation.m
@@ -2,11 +2,11 @@ addpath('Sourced Functions')
 %%
 
 % Gamma Tubulin
-RAW = imread('GammaTub_02_merge_cropped.png');
-HairCells = ~imread('GammaTub_02_merge_cropped_HairCells.bmp');
-CellBounds = imread('GammaTub_02_merge_cropped_Bounds_Skel.bmp');
-NotCells = ~imread('GammaTub_02_merge_cropped_NotCells.bmp');
-BasalBodies = ~imread('GammaTub_02_merge_cropped_BB.bmp');
+% RAW = imread('GammaTub_02_merge_cropped.png');
+% HairCells = ~imread('GammaTub_02_merge_cropped_HairCells.bmp');
+% CellBounds = imread('GammaTub_02_merge_cropped_Bounds_Skel.bmp');
+% NotCells = ~imread('GammaTub_02_merge_cropped_NotCells.bmp');
+% BasalBodies = ~imread('GammaTub_02_merge_cropped_BB.bmp');
 
 % Pericentrin Labeled
 % RAW = imread('GammaTub_02_merge_cropped.png');
@@ -15,16 +15,17 @@ BasalBodies = ~imread('GammaTub_02_merge_cropped_BB.bmp');
 % NotCells = ~imread('GammaTub_02_merge_cropped_NotCells.bmp');
 % BasalBodies = ~imread('GammaTub_02_merge_cropped_BB.bmp');
 
-[imx,imy] = size(HairCells);
 
-SupportCells = true(imx,imy)-HairCells-NotCells;
+
+% SupportCells = true(imx,imy)-HairCells-NotCells;
 
 %% New section
-HairCells =
-SupportCells =
-CellBounds =
-BasalBodies =
-
+root = 'GammaTub_02';
+HairCells = imread(strcat(root,'_HairCells.bmp'));
+SupportCells = imread(strcat(root,'_SupportCells.bmp'));
+CellBounds = imread(strcat(root,'_CellBoundaries.bmp'));
+PolarBodies = imread(strcat(root,'_PolarBodies.bmp'));
+[imx,imy] = size(HairCells);
 %% Refine Cell Selections
 sdisk = strel('disk',1);
 CellBounds = imdilate(CellBounds,sdisk);
@@ -40,10 +41,10 @@ HairCells = imclearborder(logical(HairCells));
 SupportCells = imclearborder(logical(SupportCells));
 
 %% Extract Cell params
-[HairCellProps,ImDat] = GetPropsAndIms(BasalBodies,HairCells);
+[HairCellProps,ImDat] = GetPropsAndIms(PolarBodies,HairCells);
 [HairCellProps] = BBOrient(HairCellProps,HairCellProps.Im,'BB');
 HairCellProps.Type = repmat('H',[height(HairCellProps) 1]);
-[SupportCellProps,ImDat] = GetPropsAndIms(BasalBodies,SupportCells);
+[SupportCellProps,ImDat] = GetPropsAndIms(PolarBodies,SupportCells);
 [SupportCellProps] = BBOrient(SupportCellProps,SupportCellProps.Im,'BB');
 SupportCellProps.Type = repmat('S',[height(SupportCellProps) 1]);
 %%Merge

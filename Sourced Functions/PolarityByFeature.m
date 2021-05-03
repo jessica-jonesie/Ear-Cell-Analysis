c74a091996544120749083a8262f5c006e3d8c1c
Connor <connor.healy@utah.edu>
2021-05-03 11:06:38 -0600

    Generate a polarity map on a cell by cell basis with respect some physiological feature. This is a good method to determine if a polar body is biased towards a one side of a cell due to the influence of some feature e.g. the LPR.

---
+++ Sourced Functions/PolarityByFeature.m
@@ -0,0 +1,40 @@
+function [polarities,EdgePts,EdgeLine] = PolarityByFeature(CellMask,FeatureMask,varargin)
+%POLARITYBYFEATURE Get polarity map based upon a feature that cells orient
+%with respect to. 
+%   Detailed explanation goes here
+%% parse inputs
+p = inputParser;
+addRequired(p,'CellMask',@islogical);
+addRequired(p,'FeatureMask',@islogical);
+addParameter(p,'Adjust2Center',false,@islogical);
+%%
+[EdgePts,EdgeLine]=Mask2EdgePts(FeatureMask);
+IMap = InfluenceMap(EdgeLine);
+FilledMask = imfill(CellMask,'holes');
+[NormImage]= NormByBWComp(FilledMask,IMap);
+
+% % Adjust to center
+% [~,~,ctrIm] = BWVisualCenter(FilledMask);
+% 
+% ctrPols = NormImage(ctrIm);
+% maskL = bwlabel(FilledMask);
+% ctrInd = maskL(ctrIm);
+% PolsROrd = [NaN;ctrPols(ctrInd)];
+% ctrPolMap = PolsROrd(maskL+1);
+% 
+% %less than
+% ltmap = NormImage.*(NormImage<ctrPolMap);
+% shiftltmap = (ltmap./ctrPolMap)*0.5;
+% 
+% %greater than
+% gtmap = NormImage.*(NormImage>=ctrPolMap);
+% shiftgtmap = ((gtmap-ctrPolMap)./(1-ctrPolMap))*0.5 +0.5;
+% 
+% %Merge
+% NormImage(NormImage<ctrPolMap) = shiftltmap(NormImage<ctrPolMap);
+% NormImage(NormImage>=ctrPolMap) = shiftgtmap(NormImage>=ctrPolMap);
+
+polarities = 2*abs(NormImage-0.5).*FilledMask;
+polarities(~CellMask)=NaN;
+end
+

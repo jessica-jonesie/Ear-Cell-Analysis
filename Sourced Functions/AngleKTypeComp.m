1690ccfaf29fcbf04dc6abd6169caa866dd351e4
Connor <connor.healy@utah.edu>
2021-05-14 15:14:55 -0600

    Get Angle K values for all permutations of cell type e.g. hair to hair, hair to support, support to hair, support to support.

---
+++ Sourced Functions/AngleKTypeComp.m
@@ -0,0 +1,32 @@
+function [K,Ori,KsimMax,KsimMin,name] = AngleKTypeComp(scales,props,splitvar)
+%UNTITLED12 Summary of this function goes here
+%   Detailed explanation goes here
+
+[TypeID,ntypes,types] = GetTypeIDs(props,splitvar);
+
+for k =1:ntypes
+    hvec{k}.origin = props.Center(TypeID{k},:);
+    hvec{k}.angle = props.NormOrientationR(TypeID{k});
+    hvec{k}.magnitude = ones(sum(TypeID{k}),1);
+end
+
+% Compute AngleK stats 
+alpha = 0.01;
+
+for n=1:ntypes
+    for m=1:ntypes
+        [K{n,m},Ori{n,m}] = AngleK(scales,hvec{n},hvec{m}); 
+        [~,KsimMax{n,m},KsimMin{n,m}] = AngleK_Env(scales,hvec{n},alpha,'vecB',hvec{m});
+        name{n,m} = strcat(types{n},' to ',types{m});
+    end
+end
+
+% reshape to column array
+shapmat = [ntypes.^2 1];
+K = reshape(K,shapmat);
+Ori = reshape(Ori,shapmat);
+KsimMax = reshape(KsimMax,shapmat);
+KsimMin = reshape(KsimMin,shapmat);
+name = reshape(name,shapmat);
+end
+

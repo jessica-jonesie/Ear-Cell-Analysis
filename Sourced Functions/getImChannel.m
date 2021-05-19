8a39324f50e7a07437bb7f3a300fbe3185566ec2
Connor <connor.healy@utah.edu>
2021-05-19 17:04:50 -0600

    Initial commit. Isolate a target channel of an image.

---
+++ Sourced Functions/getImChannel.m
@@ -0,0 +1,14 @@
+function [imgOut] = getImChannel(imgIn,chann)
+%GETIMCHANNEL get a specific channel of an RGB image. 
+%   Detailed explanation goes here
+if strcmpi(chann,'r')||strcmpi(chann,'red')||chann==1
+    imgOut = imgIn(:,:,1);
+elseif strcmpi(chann,'g')||strcmpi(chann,'green')||chann==2
+    imgOut = imgIn(:,:,2);
+elseif strcmpi(chann,'b')||strcmpi(chann,'blue')||chann==3
+    imgOut = imgIn(:,:,3);
+else
+    error('Invalid channel type. Must be R, G, or B.')
+end
+end
+

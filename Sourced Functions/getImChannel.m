68f1760780ed494405724506de28d0b0482cd75c
Connor <connor.healy@utah.edu>
2021-06-09 15:39:11 -0600

    Gray channel option now outputs the rgb image converted to grayscale.

--- Sourced Functions/getImChannel.m
+++ Sourced Functions/getImChannel.m
@@ -8,8 +8,9 @@ elseif strcmpi(chann,'g')||strcmpi(chann,'green')||chann(1)==2
 elseif strcmpi(chann,'b')||strcmpi(chann,'blue')||chann(1)==3
     imgOut = imgIn(:,:,3);
 elseif strcmpi(chann,'gray')||strcmpi(chann,'all')
+    imgOut = rgb2gray(imgIn);
 else
-    error('Invalid channel type. Must be R, G, or B.')
+    error('Invalid channel type. Must be R, G, B, or Gray.')
 end
 end
 

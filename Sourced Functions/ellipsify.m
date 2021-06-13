b4e27bde6cbeb90873074b35071f81f00de145bf
Connor <connor.healy@utah.edu>
2021-06-13 12:49:24 -0600

    Convert binary components into elliptical approximations

---
+++ Sourced Functions/ellipsify.m
@@ -0,0 +1,10 @@
+function [ellipseim] = ellipsify(im)
+%Ellipsify Convert binary components to elliptical approximations
+%   Detailed explanation goes here
+imRez = size(im);
+
+props = regionprops('table',im,{'Centroid','MajorAxisLength','MinorAxisLength','Orientation'});
+
+ellipseim = bwEllipse(imRez,props.Centroid,props.MajorAxisLength,props.MinorAxisLength,props.Orientation);
+end
+
